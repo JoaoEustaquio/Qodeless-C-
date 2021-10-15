c92e679bb994a4f961ad38b285bfbafd5dba5deb
Joao_Godinho <git.qodeless@outlook.com>
2021-10-15 14:35:21 -0300

    Implement Random Number

--- SquidGame/SquidGame/Program.cs
+++ SquidGame/SquidGame/Program.cs
@@ -6,33 +6,43 @@ namespace SquidGame
     {
         static void Main(string[] args)
         {
-            string mensagem;
-            int numero = 111;
-            string rota = "Rota";
+            Random random = new Random();
+            int number = random.Next(001, 999);
+
+            string rota = "";
 
             //Nome
-            Console.WriteLine(mensagem = "Olá jogador n°" + numero + ", precione ENTER para iniciar o jogo");
+            Console.WriteLine("Olá jogador n°" + number + ". Nós vamos jogar o jogo da ponte mortal. Escolha a rota que deseja seguir e veremos o seu destino. Precione ENTER para iniciar o jogo.");
             Console.ReadLine();
             Console.Clear();
 
-            Console.WriteLine(mensagem = "Escolha um dos três caminhos para seguir, digitando 1, 2 OU 3!");
-            Console.WriteLine(rota + "1");
-            Console.WriteLine(rota + "2");
-            Console.WriteLine(rota + "3");
-            Console.ReadLine();
+            Console.WriteLine("Escolha um dos três caminhos para seguir, digitando 1, 2 OU 3!");
+            Console.WriteLine("Rota 1");
+            Console.WriteLine("Rota 2");
+            Console.WriteLine("Rota 3");
+            Console.Write("Selecione a rota: ");
 
-            //if (rota = 1)
-            //{
-            //    Console.WriteLine("Você passou, escolha seu próximo caminho.");
-            //    Console.ReadLine();
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Você morreu! Precione R para REINICIAR o jogo.");
-            //    Console.ReadLine();
-            //}
+            rota = Console.ReadLine();
+            Console.Clear();
+            switch (rota.ToUpper())
+            {
+                case "1":
+                    
+                    break;
 
-            //Console.Clear();
+                case "2":
+                    
+                    break;
+
+                case "3":
+                    
+                    break;
+
+                case "4":
+
+                    break;
+            }
+
         }
     }
 }
