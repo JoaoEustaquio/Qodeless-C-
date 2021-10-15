35f22a873422bd19b459f0b54dc153dd4d1151d4
Joao_Godinho <git.qodeless@outlook.com>
2021-10-15 17:10:27 -0300

    game semi pronto

--- SquidGame/SquidGame/Program.cs
+++ SquidGame/SquidGame/Program.cs
@@ -6,43 +6,55 @@ namespace SquidGame
     {
         static void Main(string[] args)
         {
+            var decisao = "";
+            string[] rota = { "Viveu", "Morreu" };
+
             Random random = new Random();
             int number = random.Next(001, 999);
-
-            string rota = "";
+            int index = random.Next(rota.Length);
+            
 
             //Nome
             Console.WriteLine("Olá jogador n°" + number + ". Nós vamos jogar o jogo da ponte mortal. Escolha a rota que deseja seguir e veremos o seu destino. Precione ENTER para iniciar o jogo.");
             Console.ReadLine();
             Console.Clear();
 
-            Console.WriteLine("Escolha um dos três caminhos para seguir, digitando 1, 2 OU 3!");
-            Console.WriteLine("Rota 1");
-            Console.WriteLine("Rota 2");
-            Console.WriteLine("Rota 3");
-            Console.Write("Selecione a rota: ");
+            var sair = false;
 
-            rota = Console.ReadLine();
-            Console.Clear();
-            switch (rota.ToUpper())
-            {
-                case "1":
-                    
+            for (int i = 1; i <= 5; i++) {
+                Console.WriteLine("");
+                Console.WriteLine("Escolha um dos três caminhos para seguir, digitando 1, 2 OU 3!");
+                Console.WriteLine("Etapa " + i + " de 5");
+                Console.WriteLine("Rota 1");
+                Console.WriteLine("Rota 2");
+                Console.WriteLine("Rota 3");
+                Console.Write("\r\nSelecione a rota: ");
+            
+
+                decisao = Console.ReadLine();
+                switch (decisao.ToUpper())
+                {
+                    case "1":
+                        Console.WriteLine("Você " + rota[index]);
+                        if (index == 1) { sair = true; }
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Você " + rota[index]);
+                        if (index == 1) { sair = true; }
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Você " + rota[index]);
+                        if (index == 1) { sair = true; }
+                        break;
+                }
+                Console.WriteLine("");
+                if (sair)
+                {
                     break;
-
-                case "2":
-                    
-                    break;
-
-                case "3":
-                    
-                    break;
-
-                case "4":
-
-                    break;
+                }
             }
-
         }
     }
 }
